609c8fa757e2ace0d260f6efe98678118a71d38a
Stefan Eng <stefaneng13@gmail.com>
2014-04-21 10:53:26 -0700

    Successfully parsers meta-data and text

---
+++ src/main/java/ExampleTika.java
@@ -0,0 +1,57 @@
+import org.apache.tika.Tika;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.SAXException;
+
+import java.io.*;
+
+/**
+ * Created by stefan on 4/21/14.
+ */
+public class ExampleTika {
+    public static void main(String[] args) {
+        File file = new File("src/main/resources/test.pdf");
+        InputStream is = null;
+        try {
+            is = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Metadata metadata = new Metadata();
+        BodyContentHandler ch = new BodyContentHandler();
+        AutoDetectParser parser = new AutoDetectParser();
+
+        String mimeType = null;
+        try {
+            mimeType = new Tika().detect(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        metadata.set(Metadata.CONTENT_TYPE, mimeType);
+
+        try {
+            parser.parse(is, ch, metadata, new ParseContext());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (TikaException e) {
+            e.printStackTrace();
+        }
+        try {
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < metadata.names().length; i++) {
+            String item = metadata.names()[i];
+            System.out.println(item + " -- " + metadata.get(item));
+        }
+
+        System.out.println(ch.toString());
+    }
+}
